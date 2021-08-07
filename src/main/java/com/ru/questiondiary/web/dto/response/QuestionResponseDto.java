102259cfe2b1a711e6c0c2d461e797eed326c1b8
Kirill <grosukirill009@gmail.com>
2021-08-07 23:50:56 +0300

    BE #10. Better architecture for response Dto's.

--- src/main/java/com/ru/questiondiary/web/dto/response/QuestionResponseDto.java
+++ src/main/java/com/ru/questiondiary/web/dto/response/QuestionResponseDto.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class QuestionResponseDto {
+    private final Boolean status = true;
     private QuestionDto data;
 
     public QuestionResponseDto(QuestionDto data) {
