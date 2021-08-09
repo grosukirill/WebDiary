b14b8d44d3f5cee3aa005009b991d6d6cbb9ba66
Kirill <grosukirill009@gmail.com>
2021-08-09 21:36:41 +0300

    BE #14. Resolved conflicts.

--- src/main/java/com/ru/questiondiary/web/controller/GlobalControllerAdvice.java
+++ src/main/java/com/ru/questiondiary/web/controller/GlobalControllerAdvice.java
@@ -33,8 +33,7 @@ public class GlobalControllerAdvice {
         ErrorDto errorDto = new ErrorDto(e.getMessage(), LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
         ErrorResponse response = new ErrorResponse(errorDto);
         return ResponseEntity.status(HttpStatus.OK).body(response);
-<<<<<<< HEAD
-=======
+
     }
 
     @ExceptionHandler
@@ -49,6 +48,5 @@ public class GlobalControllerAdvice {
         ErrorDto errorDto = new ErrorDto(e.getMessage(), LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
         ErrorResponse response = new ErrorResponse(errorDto);
         return ResponseEntity.status(HttpStatus.OK).body(response);
->>>>>>> 0fc03ef5aed58a6e8a166b4a5e086e8d0684dc32
     }
 }
