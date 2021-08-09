96868a7a0c8975b2020c082ac0547d4d1bf4a92b
Denis <denislupookov@gmail.com>
2021-08-09 21:21:03 +0300

    Added main page

--- frontend/diary/pages/_app.js
+++ frontend/diary/pages/_app.js
@@ -1,7 +1,37 @@
-import '../styles/globals.css'
+import React from 'react';
+import App from 'next/app';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { Provider } from 'react-redux';
+import store from '../store/index';
+
+import Layout from '../app/layout/Layout';
+
+import '../assets/css/global_auth.css'
+import '../assets/css/global.css'
+
+class MyApp extends App {
+
+  static async getStaticProps({ Component, ctx }) {
+    const pageProps = Component.getStaticProps ? await Component.getStaticProps(ctx) : {};
+
+    return { pageProps: pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Provider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
+    );
+  }
+
 }
 
-export default MyApp
+const makeStore = () => store;
+
+//withRedux wrapper that passes the store to the App Component
+export default MyApp;
