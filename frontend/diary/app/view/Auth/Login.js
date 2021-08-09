15c3ac7d02a94b60e30fabba3da7020fcdd2fe8a
Denis <denislupookov@gmail.com>
2021-08-09 21:20:27 +0300

    Added login page

---
+++ frontend/diary/app/view/Auth/Login.js
@@ -0,0 +1,66 @@
+import Banner from '../../components/View/Auth/Banner'
+import Input from '../../components/View/Auth/Input'
+import Person from '../../components/svg/Auth/Person'
+import Password from '../../components/svg/Auth/Password'
+import Checkbox from '../../components/View/Auth/Checkbox'
+import Button from '../../components/View/Auth/Button'
+
+const Login = (props) => {
+    const errors = props.getAuthErrors()
+    return (
+        <div
+            className={(props.props.data.loginAnimation ? "login_appear " : "")
+                + (props.state.anim ? "auth_animation_login " : "")
+                + "auth"}>
+            <div className="auth_container login_auth">
+                <span className="auth_title_container">Вход в Дневник Вопросов</span>
+                <div className="auth_input_blocks">
+                    <Input
+                        icon={<Person />}
+                        type="email"
+                        placeholder="Email"
+                        value={props.state.user}
+                        getRef={ref => { props.setRef(ref) }}
+                        onChange={e => props.changeState("user", e.target.value)}
+                        dataId="user"
+                    />
+                    <Input
+                        icon={<Password />}
+                        value={props.state.password}
+                        onChange={e => props.changeState("password", e.target.value)}
+                        type={props.state.hidePassword ? "password" : "text"}
+                        placeholder="Password"
+                        showEye={props.state.hidePassword}
+                        getRef={ref => { props.setRef(ref) }}
+                        password={() => props.changeState("hidePassword", !props.state.hidePassword)}
+                        dataId="password"
+                    />
+
+                    {errors.status ? (
+                        <div className="erorr_text_block">
+                            <span>{errors.message}</span>
+                        </div>
+                    ) : ""}
+
+                    <Button
+                        onClick={props.signIn}
+                        // loading={props.props.auth.loading}
+                        title="Войти"
+                    />
+                </div>
+
+                <div className="login_bottom_container">
+                    <div className="no_account_block">
+                        <span className="title_account">У вас еще нет аккаунта?</span>
+                        <span className="link_account" onClick={props.changeAuthPage}>
+                            Зарегистрируйтесь
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <Banner />
+        </div>
+    )
+}
+
+export default Login;
