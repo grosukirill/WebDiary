c892b8b05b29b28d94d968a9b7d18cc9beea13ab
Denis <denislupookov@gmail.com>
2021-08-09 21:19:38 +0300

    Added register page

---
+++ frontend/diary/pages/register.js
@@ -0,0 +1,11 @@
+import RegisterController from "../app/controller/Auth/Register"
+
+const Register = () => {
+    return (
+        <div>
+            <RegisterController />
+        </div>
+    )
+}
+
+export default Register;
