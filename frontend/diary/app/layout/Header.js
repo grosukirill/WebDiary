b36eecc13b7167328d756da64ed27daa64b5df59
Denis <denislupookov@gmail.com>
2021-08-09 21:21:44 +0300

    Added layouts

---
+++ frontend/diary/app/layout/Header.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+const Header = () => {
+    return (
+        <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+            <link
+                href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap"
+                rel="stylesheet"
+            />
+        </Head>
+    )
+}
+
+export default Header;
