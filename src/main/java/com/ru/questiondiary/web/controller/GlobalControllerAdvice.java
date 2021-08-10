78963cbb3d24eb423cc291f7e8790432f72ae94c
Kirill <grosukirill009@gmail.com>
2021-08-10 21:59:38 +0300

    BE #17. Added `BadCredentialException` handler in GlobalControllerAdvice.java.

--- src/main/java/com/ru/questiondiary/web/controller/GlobalControllerAdvice.java
+++ src/main/java/com/ru/questiondiary/web/controller/GlobalControllerAdvice.java
@@ -5,6 +5,7 @@ import com.ru.questiondiary.web.dto.ErrorDto;
 import com.ru.questiondiary.web.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,6 +17,13 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler
     public ResponseEntity<?> handleAuthenticationException(AuthenticationFailedException e) {
+        ErrorDto errorDto = new ErrorDto(e.getMessage(), LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+        ErrorResponse response = new ErrorResponse(errorDto);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleBadCredentialsException(BadCredentialsException e) {
         ErrorDto errorDto = new ErrorDto(e.getMessage(), LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
         ErrorResponse response = new ErrorResponse(errorDto);
         return ResponseEntity.status(HttpStatus.OK).body(response);
