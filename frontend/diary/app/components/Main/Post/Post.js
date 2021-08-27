76295a9efe235693fd8e766d4bba7b4eb28354ba
Denis <denislupookov@gmail.com>
2021-08-27 17:23:02 +0300

    Added component for post

---
+++ frontend/diary/app/components/Main/Post/Post.js
@@ -0,0 +1,104 @@
+import Image from 'next/image'
+import Horiz from '../../svg/Post/Horiz'
+import Vote from '../../svg/Vote'
+import DownVote from '../../svg/DownVote'
+import MessageSvg from '../../svg/Post/Message'
+import Bookmark from '../../svg/Post/Bookmark'
+import DropDown from '../DropDown/Dropdown'
+import { connect } from 'react-redux'
+
+import Questions from '../../../../services/Questions'
+import { useState } from 'react'
+
+const Post = ({ title, author, date, count, key, id, ...props }) => {
+    const [countVotes, setCount] = useState(props.votes)
+    const [voted, setVotes] = useState(props.voted)
+
+    const votePost = (action) => {
+        const data = {
+            vote: action,
+            questionId: id
+        }
+        const req = {
+            body: JSON.stringify(data)
+        }
+        Questions.voteQuestion(props.auth.token, req).then(res => {
+            if (res.status) {
+                console.log(res)
+                setCount(res.data.votes)
+                setVotes(res.data.voted)
+            }
+        })
+    }
+
+    return (
+        <div className="post_component">
+            <div className="vote_post_block">
+                <Vote
+                    onClick={() => votePost(1)}
+                    className={voted === true ? "voted_icon" : ""}
+                />
+                <div className="vote_count_block">
+                    <span>{countVotes}</span>
+                </div>
+                <DownVote
+                    onClick={() => votePost(-1)}
+                    className={voted === false ? "downvote" : ""}
+                />
+            </div>
+            <div className="post_block">
+                <div className="top_post_author_block">
+                    <div className="main_top_author_block">
+                        <Image
+                            src="/avatar.png"
+                            alt="Picture"
+                            width={38}
+                            height={38}
+                        />
+                        <div className="post_author_info">
+                            <span className="post_author_title">{author}</span>
+                            <span className="post_author_date">{date}</span>
+                        </div>
+                    </div>
+                    <DropDown
+                        children={
+                            <div className="post_horiz_line">
+                                <Horiz />
+                            </div>
+                        }
+                        dataId={"post_" + key}
+                        data={
+                            [
+                                {
+                                    name: "Пожаловаться"
+                                }
+                            ]
+                        }
+                    />
+                </div>
+
+                <div className="main_post_block">
+                    <span className="question_title">{title}</span>
+                </div>
+
+                <div className="end_post_block">
+                    <div className="right_post_icons_block">
+                        <MessageSvg />
+                        <div className="bookmark_post_icon">
+                            <Bookmark />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        data: state.mainReducer,
+        auth: state.authReducer
+    }
+}
+
+export default connect(mapStateToProps)(Post);
